84af839578c7ed7e191bf08793f5af05d3ffe96e
Bastien-N <64208329+Bastien-N@users.noreply.github.com>
2021-09-29 12:15:07 +0200

    Packages and data loading

---
+++ analysis_RF.R
@@ -0,0 +1,24 @@
+#----------------#
+#   User input   #
+#----------------#
+dat <- "data_change.csv" #Which version of the cleaned dataset to be used
+#-----------------------#
+#   Loading libraries   #
+#-----------------------#
+packages <- c("ggplot2","randomForest")
+if (!requireNamespace("BiocManager", quietly = TRUE)){
+  install.packages("BiocManager")
+  BiocManager::install(version = "3.13") 
+}
+
+for (p in packages){
+  if (!require(p, character.only = TRUE)){
+    BiocManager::install(p,update = FALSE)
+    if(!require(p,character.only = TRUE)) {stop("Package not found")}
+  }
+}
+
+#------------------#
+#   Loading data   #
+#------------------#
+data <- read.csv(dat)
